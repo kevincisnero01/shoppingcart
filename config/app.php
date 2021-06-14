7083dbdc2417c2b3b7e39d4d971254388a0d2a7e
kevincisnero01 <kevingabriel18@gmail.com>
2021-06-14 13:21:09 -0430

    remove paquete darryldecode/shoppingcart

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
          * Package Service Providers...
          */
 
-        Darryldecode\Cart\CartServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,7 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Cart' => Darryldecode\Cart\Facades\CartFacade::class,
 
     ],
 
